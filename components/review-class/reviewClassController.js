d131e0d6c28f10db89ccb0e27497ee658919a1a5
Hope Casey-Allen <hcaseyal@stanford.edu>
2017-11-28 15:46:31 -0800

    WIP. Review form sends dummy request to server, gets response

--- components/review-class/reviewClassController.js
+++ components/review-class/reviewClassController.js
@@ -1,6 +1,21 @@
-
 app.controller('ReviewClassController', ['$scope', '$routeParams', function($scope, $routeParams) {
 	$scope.selectedClass = $routeParams.classID;
 	$scope.main.displayHeader = true;
 	$scope.main.displayFullHeader = false; 
 }]);
+
+function submitReview() {
+	let xhttp = new XMLHttpRequest();
+	let url = "/reviewClass";
+	let params = JSON.stringify({review: "CS106a was a great class."});
+	xhttp.open("POST", url, true);
+
+	//Send the proper header information along with the request
+	xhttp.setRequestHeader("Content-type", "application/json");
+
+	xhttp.onload = () => {
+		console.log(xhttp.responseText);
+	}
+	xhttp.send(params);
+}
+
--- webServer.js
+++ webServer.js
@@ -1,7 +1,23 @@
-
+var bodyParser = require('body-parser');
+var http = require('http');
 var express = require('express'); 
 var portno = 3000;   // Port number to use
 var app = express(); 
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+	// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.static(__dirname));
 
@@ -10,4 +26,7 @@ var server = app.listen(portno, function () {
 	console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
 });
 
-
+app.post('/reviewClass', function (req, res) {
+	console.log(req.body.review);
+	res.send("Received post request");
+});
