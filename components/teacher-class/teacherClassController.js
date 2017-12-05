07117fad30118f018bc0022a71decdeff79a7ed1
Amy Liu <liuamyj@stanford.edu>
2017-12-05 14:00:38 -0800

    redirect to teacher home when clicking on CLassLens from teacher pages

--- components/teacher-class/teacherClassController.js
+++ components/teacher-class/teacherClassController.js
@@ -5,7 +5,7 @@ app.controller('TeacherClassController', ['$scope', '$routeParams', '$route', fu
 	$scope.selectedClass = $routeParams.classID;
 	$scope.main.displayHeader = true;
 	$scope.main.displayFullHeader = false;
-	$scope.main.selectedButton = 'explore';
+	$scope.main.selectedButton = 'teacher';
 	$scope.displayFilters = false;
 	$scope.modalOn = false;
 	$scope.classRatingLabels = ['Very Useful', '', '', '', 'Not Useful']; 
