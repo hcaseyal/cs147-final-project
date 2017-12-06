35ca64011e53adc77f2d5e9ed3ecdfcd183ce62c
Amy Liu <liuamyj@stanford.edu>
2017-12-06 00:56:37 -0800

    teacher form preview

--- components/edit-form/editFormController.js
+++ components/edit-form/editFormController.js
@@ -5,16 +5,31 @@ app.controller('EditFormController', ['$scope', '$routeParams', function($scope,
 	$scope.main.displayFullHeader = false; 
 	$scope.main.selectedButton = 'teacher'; 
 
+	$scope.preview = false;
 	$scope.selectedQuarter = ''; 
 	$scope.selectedYear = ''; 
 	$scope.quarterList = ['Fall', 'Winter', 'Spring', 'Summer']; 
 	$scope.yearList = ['13-14', '14-15', '15-16', '16-17'];
 	$scope.skillTags = [];
 
+	$scope.slider = {
+	    value: 3,
+	    options: {
+	    	showTicksValues: true,
+	    	stepsArray: [
+		    	{value: 1, legend: 'Not useful'},
+		    	{value: 2},
+		    	{value: 3},
+		    	{value: 4},
+		    	{value: 5, legend: 'Very useful'}
+			]
+	    }
+	};
+
 	$scope.submitCourseEdits = function() {
-		var classSkills = $scope.skillTags.map(x => x.text);
+		$scope.classSkills = $scope.skillTags.map(x => x.text);
 		
-		let post = {classSkills: classSkills,
+		let post = {classSkills: $scope.classSkills,
 					selectedQuarter: $scope.selectedQuarter,
 					selectedYear: $scope.selectedYear,
 					classID: $scope.selectedClass,
@@ -27,5 +42,11 @@ app.controller('EditFormController', ['$scope', '$routeParams', function($scope,
 		.catch(error => {
 			console.log(error);
 		});
+	}
+
+	$scope.togglePreview = function() {
+		$scope.preview = !$scope.preview;
+		$scope.classSkills = $scope.skillTags.map(x => x.text);
+		$scope.classYear = $scope.selectedQuarter + ' ' + $scope.selectedYear;
 	}
 }]);
