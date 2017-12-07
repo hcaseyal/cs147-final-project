6f1cc1c3d46d5bcabe3fe90164759d06e96403c2
Amy Liu <liuamyj@stanford.edu>
2017-12-06 21:11:00 -0800

    added typeahead-on-select callback

--- components/header/headerController.js
+++ components/header/headerController.js
@@ -1,11 +1,25 @@
 
-app.controller('HeaderController', ['$scope', function($scope) {
-	$scope.searchGraph = ['CS106A', 'CS107', 'Software Developer', 'Recursion', 'Java', 'Data Scientist', 'Python', 'MATH51'];
+app.controller('HeaderController', ['$scope', '$location', function($scope, $location) {
+	$scope.searchCareers = ['Software Developer', 'Data Scientist']; 
+	$scope.searchSkills = ['Recursion', 'Java', 'Python'];
 	$scope.searchClasses = ['CS106A', 'MATH51', 'BIO150', 'CS142', 'CS145', 'MATH110', 'ENGR50', 'STS200L', 
 							'CS255', 'CS124', 'COMM154', 'CS168', 'CS247', 'CS161'];
+	$scope.searchGraph = $scope.searchCareers.concat($scope.searchSkills).concat($scope.searchClasses);
+	$scope.searchTeacherClasses = ['CS106A', 'CS107', 'CS109']; 
 
+	// after selecting a class/career/skill, should zoom in to the appropriate node
 	$scope.onSelect = function ($item, $model, $label) {
 		// $item is the selected item from the search dropdown
 	    console.log($item);
+	};
+
+	// after selecting a class from the class search bar, should redirect to that class page
+	$scope.onSelectClass = function ($item, $model, $label) {
+		$location.path('class/' + $item);
+	};
+
+	// (teacher view) after selecting a class from the class search bar, should redirect to that class page 
+	$scope.onSelectTeacherClass = function ($item, $model, $label) {
+		$location.path('teacher-class/' + $item);
 	};
 }]);
